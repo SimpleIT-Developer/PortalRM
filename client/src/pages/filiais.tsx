f32cd40f47a19a3e6bc0c848017b3d24a6996c88
SimpleIT - Developer <simpleit.solucoes@gmail.com>
2025-12-16 17:13:45 -0300

    Add CNPJ formatting

--- client/src/pages/filiais.tsx
+++ client/src/pages/filiais.tsx
@@ -28,6 +28,19 @@ export default function FiliaisPage() {
     }
   };
 
+  const formatCNPJ = (cnpj: string | undefined) => {
+    if (!cnpj) return '-';
+    
+    // Remove caracteres não numéricos
+    const numbers = cnpj.replace(/\D/g, '');
+    
+    // Verifica se tem 14 dígitos
+    if (numbers.length !== 14) return cnpj;
+    
+    // Formata: XX.XXX.XXX/0001-XX
+    return numbers.replace(/^(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})$/, "$1.$2.$3/$4-$5");
+  };
+
   useEffect(() => {
     fetchBranches();
   }, []);
@@ -93,7 +106,7 @@ export default function FiliaisPage() {
                       <TableCell>{branch.Code}</TableCell>
                       <TableCell>{branch.Description}</TableCell>
                       <TableCell>{branch.Title || '-'}</TableCell>
-                      <TableCell>{branch.CGC || '-'}</TableCell>
+                      <TableCell>{formatCNPJ(branch.CGC)}</TableCell>
                     </TableRow>
                   ))}
                 </TableBody>
